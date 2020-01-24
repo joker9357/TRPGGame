0fdca30baf0222f6011009a125a17b2740d47625
tingyugu <tingyugu@tingyugudeMacBook-Pro.local>
2020-01-24 13:38:14 +0800

    step up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from '../component/nav-bar/nav-bar.component';
+import { MaterialDesignModule } from '../module/material-design/material-design.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/module/material-design/material-design.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports:[
+    MatToolbarModule
+  ]
+})
+export class MaterialDesignModule { }
